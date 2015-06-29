fa9f26909e12fc795a76865e4e129d1e07f7b891
Scott Hardy <scott.the.hardy@gmail.com>
2015-06-29 02:42:51 -0700

    swag

--- index2.js
+++ index2.js
@@ -6,6 +6,7 @@ import xml2js from 'xml2js';
 import * as config from './config/config';
 
 import * as Map from './lib/Map';
+import * as util from './lib/util';
 
 
 var mapId = '602T';
@@ -15,4 +16,15 @@ Map.getMap(mapId)
   .then((map) => {
     map.roundCoordinates();
     console.log(JSON.stringify(map, null, 2));
+    return map;
+  })
+
+  .then((map) => {
+    map.shapes.forEach((shape) => {
+      shape.way.coordinates.forEach((point) => {
+        map.markers.forEach((marker) => {
+          if (point.lat === marker.position.lat && point.lon === marker.position.lon) console.log('Marker', marker.label, 'lies on', shape.label);
+        });
+      });
+    });
   });
