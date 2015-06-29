a1d8cf1699487b6272b96fb61fd31f9a9334355b
Scott Hardy <scott.the.hardy@gmail.com>
2015-06-29 00:16:32 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# GPS
+
+## Usage
+
+1. Make your map using [CalTopo](http://caltopo.com)
+1. Click `Export`, then `Download CalTopo Backup`
+
+## Notes
+
+* ~5x5 pixels for LAT/LON rounded to 4 decimal places
---
+++ config/config.example.js
@@ -0,0 +1,23 @@
+export const FILE_NAME = 'shasta-trinity.gpx';
+
+export const FOLDERS = {
+  Campgrounds: {
+    symbol: 'squaregreen'
+  },
+  Camps: {
+    symbol: 'squaregreen'
+  },
+  Peaks: {
+    symbol: 'trianglered'
+  },
+  Trailheads: {
+    symbol: 'squarered'
+  },
+  Water: {
+    symbol: 'circleblue'
+  }
+};
+
+export const GOOGLE_API_KEY = 'AIzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz';
+
+export const FEET_IN_METER = 3.280839895;
